da7cbfb26362dd5592fa801e640bed05a604d37b
spikehu <1214059187@qq.com>
2023-03-18 18:41:09 +0800

    解决mmap映射释放问题

--- clientFolder/Client.cpp
+++ clientFolder/Client.cpp
@@ -28,30 +28,6 @@ Client::Client(char* ip , char* port ,char* fil) //需要传入ip地址 端口�
     }
     printf("##############----map file %s to memory success --############### \n",fil);
 
-    printf("filsize:%d\n",strlen(mapMem_p));
-
-    // //打印一下映射过去的内容
-    // printf("%s",mapMem_p);
-
-    //试试把映射的内存存入新的文件中
-    // struct st_filInfo newfil;
-    // newfil.size = filInfo->size;
-    // memset(&newfil.filname,0,sizeof(newfil.filname));
-    // memcpy(&newfil.filname,"newfil.txt",sizeof("newfil.txt"));
-
-    // if(creat_fil(&newfil)==false)
-    // {
-    //     printf("create file failed\n");
-    //     exit(-1);
-    // }
-    int fd = open( "newfil.txt",O_RDWR);
-    for(int i = 0 ;i < strlen(mapMem_p) ;i++ )
-    {
-        write(fd ,mapMem_p+i,1);
-        printf("%c",*(mapMem_p+i));
-    }
-    close(fd);
-
 }
 Client::~Client()
 {
